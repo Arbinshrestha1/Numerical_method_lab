8360d4d5c54f722208f6b37f4d4c0246974a02c3
Arbinshrestha1 <83805439+Arbinshrestha1@users.noreply.github.com>
2022-11-24 18:15:45 +0545

    lab3

---
+++ Lab3/qn6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+#define f(x) 1/(1+pow(x,2))
+
+ float simpson(float a, float b){
+     float h=(b-a)/3;
+     float sum = f(a) + f(b);
+
+     for(int i=1; i<=3-1; i++){
+         float k = a+i*h;
+         sum+=3*f(k);
+     }
+     return sum*(3*h/8);
+ }
+
+ int main()
+ {
+  float lower, upper;
+
+  cout<<"Enter lower limit of integration: ";
+  cin>>lower;
+  cout<<"Enter upper limit of integration: ";
+  cin>>upper;
+
+  cout<< endl <<"Required value of integration is: "<<simpson(lower, upper);
+
+  return 0;
+ }
+
---
+++ Lab3/qn8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+#define f(x) pow(x,2)+1
+ //for n=2
+ float gauss2(){
+     float w1=1, w2=1, x1=-1/sqrt(3), x2=1/sqrt(3);
+     float integration = w1*f(x1)+w2*f(x2);
+     return integration;
+ }
+
+ //for n=3
+ float gauss3(){
+     float w1=5/9, w2=8/9, w3=5/9, x1=-sqrt(3/5), x2=0, x3=sqrt(3/5);
+     float integration = w1*f(x1)+w2*f(x2)+w3*f(x3);
+     return integration;
+ }
+
+ int main(){
+  cout<<gauss2()<<endl<<gauss3();
+  return 0;
+ }
